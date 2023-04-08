2cbaca7d5b1cc03ea036d2e4cc08ac80dcb439a6
sami0web <79028718+sami0web@users.noreply.github.com>
2023-04-08 00:28:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sami-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jazjvg)
---
+++ src/components/Body1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Body1.css';
+import UserList from './UserList';
+
+function Body1() {
+  return (
+    <div className="Body1">
+        <UserList />
+    </div>
+  );
+}
+
+export default Body1;
